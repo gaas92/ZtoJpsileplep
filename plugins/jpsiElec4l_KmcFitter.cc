c5288eb20caee90ae01125e26cfe537aae25013c
Gabriel <gaba929@gmail.com>
2020-07-07 13:04:18 -0500

    4lelec update

--- plugins/jpsiElec4l_KmcFitter.cc
+++ plugins/jpsiElec4l_KmcFitter.cc
@@ -301,12 +301,12 @@ void jpsiElec4l_KmcFitter::produce(edm::Event& iEvent, const edm::EventSetup& iS
   int nPV    = primaryVertices_handle->size();
   std::cout<< "good KFitter load" << std::endl;
   for(View<pat::Muon>::const_iterator iMuon1 = muons->begin(); iMuon1 != muons->end(); ++iMuon1){
+      std::cout << " works 304 " << std::endl;
    for(View<pat::Muon>::const_iterator iMuon2 = iMuon1+1; iMuon2 != muons->end(); ++iMuon2){
     for(View<pat::Electron>::const_iterator iElec1 = electrons->begin(); iElec1 != electrons->end(); ++iElec1){
      for(View<pat::Electron>::const_iterator iElec2 = iElec1+1; iElec2 != electrons->end(); ++iElec2){
         if(iMuon1 == iMuon2) continue;
         if(iElec1 == iElec2) continue;
-        std::cout << " works 309 " << std::endl;
         int ch_m1 = iMuon1->charge();
         int ch_m2 = iMuon2->charge();
         int ch_e1 = iElec1->charge();
