43cdb5ffebd470f3ab1358433064df42f6a6a887
Gabriel <gaba929@gmail.com>
2020-07-07 11:08:59 -0500

    4lelec update

--- test/runMc4lElec.py
+++ test/runMc4lElec.py
@@ -32,26 +32,10 @@ runEnergyCorrections=False, #corrections by default are fine so no need to re-ru
 era='2018-Prompt')
 
 
-###jspsi sequence
-process.load("HeavyFlavorAnalysis.Onia2MuMu.onia2MuMuPAT_cfi")
-process.onia2MuMuPAT.muons=cms.InputTag('slimmedMuons')
-process.onia2MuMuPAT.primaryVertexTag=cms.InputTag('offlineSlimmedPrimaryVertices')
-process.onia2MuMuPAT.beamSpotTag=cms.InputTag('offlineBeamSpot')
-process.onia2MuMuPAT.higherPuritySelection=cms.string("isGlobalMuon")
-process.onia2MuMuPAT.lowerPuritySelection=cms.string("isGlobalMuon")
-process.onia2MuMuPAT.dimuonSelection=cms.string("2.55 < mass && mass < 3.65") ## linea 149
-process.onia2MuMuPAT.addMCTruth = cms.bool(False)
-
-###Lepton filter
-process.dileptonFilter = cms.EDProducer('LeptonMcFilterElectron',
-    electronsMiniAOD        = cms.InputTag("slimmedElectrons"),
-    primaryVertices         = cms.InputTag('offlineSlimmedPrimaryVertices')
-)
-
 ####Kinematic Fit
 process.Zfitter    = cms.EDProducer("jpsiElec4l_KmcFitter",
-                          dimuon = cms.InputTag("onia2MuMuPAT"),
-                          dilepton = cms.InputTag("dileptonFilter","dilepton"),
+                          electronsMiniAOD = cms.InputTag("slimmedElectrons"),
+                          muons             = cms.InputTag("slimmedMuons"),
                           primaryVertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
                           GenParticles   = cms.InputTag("prunedGenParticles"),
                           packedGenParticles = cms.InputTag("packedGenParticles")
@@ -64,7 +48,6 @@ process.oniarootupler = cms.EDAnalyzer('Zjpsi_eeMCTupler',
            )
 
 
-process.oniaSequence = cms.Sequence(process.onia2MuMuPAT) ##No trigger matching for 2017yet
-process.leptonSequence = cms.Sequence(process.dileptonFilter*process.Zfitter)
+process.leptonSequence = cms.Sequence(process.Zfitter)
 
-process.p = cms.Path(process.egammaPostRecoSeq*process.oniaSequence*process.leptonSequence*process.oniarootupler)
+process.p = cms.Path(process.egammaPostRecoSeq*process.leptonSequence*process.oniarootupler)
