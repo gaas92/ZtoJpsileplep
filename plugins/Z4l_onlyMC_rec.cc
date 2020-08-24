f41156059606fdb99a61e1be79e4765b10caafc2
Gabriel <gaba929@gmail.com>
2020-08-24 14:24:56 -0500

    Z4l mc only gen update

--- plugins/Z4l_onlyMC_rec.cc
+++ plugins/Z4l_onlyMC_rec.cc
@@ -238,7 +238,7 @@ Z4l_onlyMC_rec::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
                             muon_pedorro.SetPtEtaPhiM(stable_dau->pt(), stable_dau->eta(), stable_dau->phi(), stable_dau->mass());
                             suma_rancia = suma_rancia + muon_pedorro;
                             std::cout << "print pa riba ..." << std::endl;
-                            printMCtreeUP(stable_dau);
+                            printMCtreeUP(stable_dau, 0);
                         }
                         else if (stable_id == 11 || stable_id == -11){
                             efromZ++;
