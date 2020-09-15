6d24b8db3be2737821db83c01bab743f2eec63d3
Gabriel <gaba929@gmail.com>
2020-09-14 22:38:18 -0500

    v7 electrons

--- plugins/jpsiElecKmcFitter.cc
+++ plugins/jpsiElecKmcFitter.cc
@@ -334,7 +334,7 @@ void jpsiElecKmcFitter::produce(edm::Event& iEvent, const edm::EventSetup& iSetu
   int nmuons = dimuons->size()*2;
   int nelecs = leptons->size();
   int nPV    = primaryVertices_handle->size();
-  /*
+  
   for (pat::CompositeCandidateCollection::const_iterator dimuon = dimuons->begin(); dimuon != dimuons->end(); ++dimuon ) {
         //Jpsi Muons
     
@@ -478,7 +478,7 @@ void jpsiElecKmcFitter::produce(edm::Event& iEvent, const edm::EventSetup& iSetu
                      
                 int lept1Mu8DiEle12 = 0;
                 int lept2Mu8DiEle12 = 0;
-                
+                /*
                 //const pat::Electron* lept1 = dynamic_cast<const pat::Electron*>(dilepton->daughter("lepton1"));
                 //const pat::Electron* lept2 = dynamic_cast<const pat::Electron*>(dilepton->daughter("lepton2"));
                 const pat::Electron* lept1 = 0;
@@ -1205,19 +1205,19 @@ void jpsiElecKmcFitter::produce(edm::Event& iEvent, const edm::EventSetup& iSetu
                 
             ZCandColl->push_back(patZ);
             std::cout<< "something has been pushed"	<< std::endl;
-			
+            */
+            reco::CompositeCandidate recoZ(0, math::XYZTLorentzVector(1, 1, 1, sqrt(3)), math::XYZPoint(0, 0, 0), 23);
+            pat::CompositeCandidate patZ(recoZ);
+            ZCandColl->push_back(patZ);
 			//} Z candidate is valid
 
 	    }
       }//end lepton
   }
-  //iEvent.put(std::move(ZCandColl),"ZCandidates");
-  */
-  reco::CompositeCandidate recoZ(0, math::XYZTLorentzVector(1, 1, 1, sqrt(3)), math::XYZPoint(0, 0, 0), 23);
-  pat::CompositeCandidate patZ(recoZ);
-  ZCandColl->push_back(patZ);
   std::cout << "is ZCandColl empty ?" << ZCandColl->empty() << std::endl;
   iEvent.put(std::move(ZCandColl),"ZCandidates");
+  
+  
   //std::cout << "jpsiElecKmcFitter is working ok" << std::endl;
 }
 
