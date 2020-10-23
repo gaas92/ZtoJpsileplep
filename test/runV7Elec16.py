fa597e119d72429a82487be5f5069818c6cd701e
Gabriel <gaba929@gmail.com>
2020-10-22 23:39:03 -0500

    test 4 Nogsf-TTB

--- test/runV7Elec16.py
+++ test/runV7Elec16.py
@@ -9,7 +9,7 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100000
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.Reconstruction_cff')
-#process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
+process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
