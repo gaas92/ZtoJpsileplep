5106b2df904ed5de237a6f2f35c20e5a3c652a67
Gabriel <gaba929@gmail.com>
2020-11-26 17:48:21 -0600

    rec myGen

--- test/runV7MyGenZJPEE.py
+++ test/runV7MyGenZJPEE.py
@@ -20,7 +20,7 @@ process.source = cms.Source("PoolSource",
    )
 
 )
-
+"""
 process.TFileService = cms.Service("TFileService",
         fileName = cms.string('Zjpsi_only_gen.root'),
 )
@@ -116,4 +116,3 @@ process.oniaSequence = cms.Sequence(process.onia2MuMuPAT) ##No trigger matching 
 process.leptonSequence = cms.Sequence(process.Zfitter)
 
 process.p = cms.Path(process.egammaPostRecoSeq*process.muonFilter*process.electonFilter*process.oniaSequence*process.leptonSequence*process.oniarootupler)
-"""
