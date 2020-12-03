defcd0da9b6ef956cc235d4fb6a7b68ff14e89c4
Gabriel <gaba929@gmail.com>
2020-12-03 11:44:43 -0600

    Tigger Test

--- plugins/ZjpsiMCTupler.cc
+++ plugins/ZjpsiMCTupler.cc
@@ -465,7 +465,7 @@ ZjpsiMCTupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    try {
      std::string const &trig = std::string("TriggerResults::HLT");//+hlTriggerResults_;
      iEvent.getByLabel(edm::InputTag(trig),hltresults1);
-     std::cout << "Handle on HLT ok " << std::endl;
+     //std::cout << "Handle on HLT ok " << std::endl;
    }
    catch ( ... )
     {
@@ -780,7 +780,7 @@ void ZjpsiMCTupler::CheckHLTTriggers(const std::vector<std::string>& TrigList){
     for (int itrig=0; itrig< ntrigs; itrig++) {
          //TString trigName = triggerNames_.triggerName(itrig);
          string trigName = TrigList.at(itrig);
-         //std::cout << "saving ..." << trigName << std::endl;
+         std::cout << "saving ..." << trigName << std::endl;
          tmptrig = (string) trigName; tmptrig +=" ";
          AllTrg += tmptrig;
     }
